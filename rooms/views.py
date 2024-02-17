efbac4c9e2822b1e883eb521237a167a5ef91922
blueconecell <95961799+blueconecell@users.noreply.github.com>
2024-02-17 18:11:20 +0900

    #16 API TESTING 대충진행

--- rooms/views.py
+++ rooms/views.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError,PermissionDenied
-from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.status import HTTP_204_NO_CONTENT,HTTP_400_BAD_REQUEST
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Room, Amenity
@@ -31,7 +31,7 @@ class Amenities(APIView):
                 AmenitySerializer(amenity).data
             )
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors,HTTP_400_BAD_REQUEST,)
 
 class AmenityDetail(APIView):
 
