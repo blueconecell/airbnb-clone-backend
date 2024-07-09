08ecde487e7434aa7a9f140cb1b6d442e08cfbf7
blueconecell <95961799+blueconecell@users.noreply.github.com>
2024-07-09 14:55:26 +0900

    poetry 버전 낮춰보기

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-pip install --upgrade pip
 
-poetry install
+pip install --upgrade pip
+pip install poetry==1.2.0
+rm poetry.lock
+poetry lock
+python -m poetry install
 pip install --force-reinstall -U setuptools
 python manage.py collectstatic --no-input
 python manage.py migrate
