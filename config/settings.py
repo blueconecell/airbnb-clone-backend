cec95e4cefb8406944f76940491be0d06c92c69c
blueconecell <95961799+blueconecell@users.noreply.github.com>
2024-07-10 12:19:40 +0900

    수정

--- config/settings.py
+++ config/settings.py
@@ -200,8 +200,8 @@ if DEBUG:
     CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
     CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
 else:
-    CORS_ALLOWED_ORIGINS = ["https://airbnb-clone-frontend-xade.onrender.com/"]
-    CSRF_TRUSTED_ORIGINS = ["https://airbnb-clone-frontend-xade.onrender.com/"]
+    CORS_ALLOWED_ORIGINS = ["https://airbnb-clone-frontend-xade.onrender.com"]
+    CSRF_TRUSTED_ORIGINS = ["https://airbnb-clone-frontend-xade.onrender.com"]
 
 
 
